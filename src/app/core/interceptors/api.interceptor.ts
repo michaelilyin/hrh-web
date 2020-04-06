d184a30581609fc169efee6af6748bac157bb067
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-06 22:18:39 +0300

    Try http

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -8,15 +8,36 @@ import {
 import {Observable} from 'rxjs';
 import {REQUEST} from '@nguniversal/express-engine/tokens';
 import {Request} from 'express';
+import {EnvironmentService} from '../services/environment.service';
+import {first, switchMap} from 'rxjs/operators';
+
+function concatUrl(host: string, path: string): string {
+  if (path.startsWith('/')) {
+    return `${host}${path}`;
+  }
+  return `${host}/${path}`;
+}
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
-  constructor(@Optional() @Inject(REQUEST) protected request?: Request) {
+  constructor(
+    private readonly environmentService: EnvironmentService,
+    @Optional() @Inject(REQUEST) private readonly request?: Request,
+  ) {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.warn(request.url);
+    if (request.url.startsWith('/v1') || request.url.startsWith('v1')) {
+      return this.environmentService.environment$.pipe(
+        first(),
+        switchMap(env => {
+          return next.handle(request.clone({
+            url: concatUrl(env.api, request.url)
+          }))
+        })
+      )
+    }
     if (this.request !== undefined && this.request !== null) {
       const host = `${this.request.protocol}://${this.request.get('host')}`;
       const slash = request.url.startsWith('/') === true ? '' : '/';
