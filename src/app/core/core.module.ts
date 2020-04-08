62eb68abee93fec5287b7174ad2bc1aa2504f28f
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-08 19:47:07 +0300

    Try to auth on server

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,25 +1,14 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ENVIRONMENT_INITIALIZER} from './services/environment.service';
-import {API_INTERCEPTOR} from './interceptors/api.interceptor';
-import {HttpClientModule} from '@angular/common/http';
-import {AUTH_INITIALIZER} from './auth/auth.initializer';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ENVIRONMENT_INITIALIZER } from './services/environment.service';
+import { API_INTERCEPTOR } from './interceptors/api.interceptor';
+import { HttpClientModule } from '@angular/common/http';
+import { AUTH_INITIALIZER } from './auth/auth.initializer';
 import { OAuthModule } from 'angular-oauth2-oidc';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    OAuthModule.forRoot()
-  ],
-  providers: [
-    ENVIRONMENT_INITIALIZER,
-    AUTH_INITIALIZER,
-
-    API_INTERCEPTOR
-  ]
+  imports: [CommonModule, HttpClientModule, OAuthModule.forRoot()],
+  providers: [ENVIRONMENT_INITIALIZER, AUTH_INITIALIZER, API_INTERCEPTOR]
 })
-export class CoreModule {
-}
+export class CoreModule {}
