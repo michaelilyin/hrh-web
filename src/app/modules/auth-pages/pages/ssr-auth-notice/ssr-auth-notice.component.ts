20b63cf9e5c6e604d94e423d1ee07bf55334dc25
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-12 13:19:54 +0300

    Try to use SSR stub for protected pages

--- src/app/modules/auth-pages/pages/ssr-auth-notice/ssr-auth-notice.component.ts
+++ src/app/modules/auth-pages/pages/ssr-auth-notice/ssr-auth-notice.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hrh-ssr-auth-notice',
   templateUrl: './ssr-auth-notice.component.html',
-  styleUrls: ['./ssr-auth-notice.component.scss']
+  styleUrls: ['./ssr-auth-notice.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SsrAuthNoticeComponent implements OnInit {
-  constructor() {}
-
   ngOnInit(): void {}
 }
