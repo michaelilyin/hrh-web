af698fb431ffef011decc156326d3872c54c0cb2
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-12 13:18:24 +0300

    Try to use SSR stub for protected pages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@ module.exports = {
   '*.ts': (files) => {
     const cwd = process.cwd();
     const names = files.map((file) => file.substring(cwd.length));
+    console.info(names.join(','))
     return `ng lint --typeCheck=true --files ${names.join(',')}`;
   },
   '*.{ts,html,scss}': 'prettier --check'
