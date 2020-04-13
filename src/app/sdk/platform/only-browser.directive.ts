b333ddd1bfbf2b9b24f798240e8b6577e81275a1
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-13 10:03:18 +0300

    Debug spinner

--- src/app/sdk/platform/only-browser.directive.ts
+++ src/app/sdk/platform/only-browser.directive.ts
@@ -6,6 +6,7 @@ import {
   Input,
   OnDestroy,
   OnInit,
+  Renderer2,
   TemplateRef,
   ViewContainerRef
 } from '@angular/core';
@@ -26,18 +27,20 @@ export class OnlyBrowserDirective implements OnInit, OnDestroy {
     private readonly viewContainerRef: ViewContainerRef,
     private readonly templateRef: TemplateRef<void>,
     private readonly platform: Platform,
-    private readonly componentFactoryResolver: ComponentFactoryResolver
+    private readonly componentFactoryResolver: ComponentFactoryResolver,
+    private readonly renderer: Renderer2
   ) {}
 
   ngOnInit(): void {
     if (this.platform.isBrowser) {
-      // this.view = this.viewContainerRef.createEmbeddedView(this.templateRef);
+      this.view = this.viewContainerRef.createEmbeddedView(this.templateRef);
     } else if (this.else != undefined) {
       if (typeof this.else === 'string') {
         const spinnerFactory = this.componentFactoryResolver.resolveComponentFactory(MatSpinner);
         this.component = this.viewContainerRef.createComponent(spinnerFactory);
         this.component.instance.color = 'accent';
         this.component.instance.diameter = 25;
+        this.renderer.addClass(this.component.instance._elementRef.nativeElement, 'ssr-spinner');
         this.component.changeDetectorRef.markForCheck();
       } else {
         this.view = this.viewContainerRef.createEmbeddedView(this.else);
