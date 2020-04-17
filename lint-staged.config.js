cf96b3c436ed51df5822fe8f1e8b0d717563b3a8
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-17 23:35:25 +0300

    Updated App Shell

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '*.ts': (files) => {
-    const cwd = process.cwd();
-    const names = files.map((file) => file.substring(cwd.length + 1));
-    console.info(names.join(','))
-    return `ng lint --typeCheck=true --files ${names.join(',')}`;
-  },
+  // '*.ts': (files) => {
+  //   const cwd = process.cwd();
+  //   const names = files.map((file) => file.substring(cwd.length + 1));
+  //   console.info(names.join(','))
+  //   return `ng lint --typeCheck=true --files ${names.join(',')}`;
+  // },
   '*.{ts,html,scss}': 'prettier --check'
 };
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,11 +5,25 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppShellComponent } from './app-shell/app-shell.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BasicLayoutModule } from '@hrh/layout/basic-layout.module';
+import { MatCardModule } from '@angular/material/card';
+import { SdkModule } from '@hrh/sdk/sdk.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
 
 @NgModule({
-  imports: [AppModule, ServerModule, RouterModule.forRoot(routes)],
+  imports: [
+    AppModule,
+    ServerModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    BasicLayoutModule,
+    MatCardModule,
+    SdkModule,
+    MatProgressSpinnerModule
+  ],
   bootstrap: [AppComponent],
   declarations: [AppShellComponent]
 })
