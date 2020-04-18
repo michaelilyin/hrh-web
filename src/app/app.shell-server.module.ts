32217d7906c0af2db4171da82bfbab481917dcf2
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-04-18 16:14:34 +0300

    Test Shell WO ssr

---
+++ src/app/app.shell-server.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BasicLayoutModule } from '@hrh/layout/basic-layout.module';
+import { MatCardModule } from '@angular/material/card';
+import { SdkModule } from '@hrh/sdk/sdk.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    BasicLayoutModule,
+    MatCardModule,
+    SdkModule,
+    MatProgressSpinnerModule
+  ],
+  bootstrap: [AppComponent],
+  declarations: [AppShellComponent]
+})
+export class AppShellServerModule {}
