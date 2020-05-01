e9bff46f1e78de20d639fdb6b60a7f1d7b566fcf
Michael Ilyin <michael.s.ilyin@gmail.com>
2020-05-01 20:39:00 +0300

    Fix dashboard houses

--- src/app/modules/dashboard/user-dashboard/user-houses/user-houses.component.ts
+++ src/app/modules/dashboard/user-dashboard/user-houses/user-houses.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { HousesService } from '@hrh/houses/_services/houses.service';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, tap } from 'rxjs/operators';
 import { CurrentHouse } from '@hrh/houses/_models/house.model';
 import { ActivatedRoute } from '@angular/router';
 import { CurrentHousesCountResolver } from '@hrh/houses/_resolver/current-houses-count.resolver';
@@ -12,7 +12,11 @@ import { CurrentHousesCountResolver } from '@hrh/houses/_resolver/current-houses
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserHousesComponent implements OnInit {
-  housesCount$ = this.activatedRoute.data.pipe(map(CurrentHousesCountResolver.extract), map(Array), shareReplay(1));
+  housesCount$ = this.activatedRoute.data.pipe(
+    map(CurrentHousesCountResolver.extract),
+    map((size) => Array(size)),
+    shareReplay(1)
+  );
   houses$ = this.housesService.getCurrentUserHouses().pipe(shareReplay(1));
 
   constructor(private readonly housesService: HousesService, private readonly activatedRoute: ActivatedRoute) {}
